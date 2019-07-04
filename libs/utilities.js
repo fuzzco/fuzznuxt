baf8e95e5446a879d1e772b94b949afd1bbe6212
Sander Moolin <sandermoolin@gmail.com>
2019-07-04 08:34:02 -0400

    ✨ Added scrollTo to utilities

--- libs/utilities.js
+++ libs/utilities.js
@@ -1,4 +1,4 @@
-import { easing } from 'popmotion'
+import { easing, styler, tween } from 'popmotion'
 const { cubicBezier } = easing
 
 export const lerp = (a, b, alpha) => {
@@ -8,3 +8,12 @@ export const lerp = (a, b, alpha) => {
 export const wait = (time = 1000) => new Promise(res => setTimeout(res, time))
 
 export const bezier = (p0, p1, p2, p3) => cubicBezier(p0, p1, p2, p3)
+
+export const scrollUp = () => {
+    const win = styler(window)
+    tween({
+        from: win.get('scrollTop'),
+        to: 0,
+        duration: 1000
+    }).start(win.set('scrollTop'))
+}
