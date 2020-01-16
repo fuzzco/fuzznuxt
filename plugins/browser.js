ec95732007769662378efbf85e7cf3551fa86d3e
Sander Moolin <sandermoolin@gmail.com>
2020-01-16 15:50:45 -0500

    Style cleanup

--- plugins/browser.js
+++ plugins/browser.js
@@ -1,4 +1,3 @@
-import Vue from 'vue'
 import { throttle } from 'lodash'
 import { autoBlur } from 'auto-blur'
 import {
@@ -9,23 +8,13 @@ import {
 
 // plugin
 export default async ({ store, route }, inject) => {
-    // polyfill
-    require('string.prototype.includes')
-
-    // size/scroll handlers
-    const onResize = () => resizeHandler(store)
-    const onScroll = () => scrollHandler(store)
-
     // setup dom listeners
-    window.addEventListener('resize', throttle(onResize, 50))
     // ~16ms is 60fps
-    window.addEventListener('scroll', throttle(onScroll, 16))
-    // watch mouse position
+    window.addEventListener('resize', throttle(() => resizeHandler(store), 16))
+    window.addEventListener('scroll', throttle(() => scrollHandler(store), 16))
     window.addEventListener('mousemove', evt => {
         mouseMoveHandler(evt, store)
     })
-
-    // detect window blur/focus
     window.onblur = () => {
         store.commit('browser/SET_WINDOW_BLURRED')
     }
@@ -35,25 +24,6 @@ export default async ({ store, route }, inject) => {
 
     // load fonts
     store.dispatch('browser/LOAD_FONTS')
-
-    // register directives
-    // Vue.directive('full-height', require('fh-components/v-full-height'))
-    // Vue.directive('reverse-hover', require('fh-components/v-reverse-hover'))
-    // Vue.directive('in-view', require('fh-components/v-in-view'))
-
-    // register components
-    // Vue.component('scroll-to', require('fh-components/scroll-to'))
-    // Vue.component('mailing-list', require('fh-components/mailing-list'))
-    // Vue.component('split-text', require('fh-components/split-text'))
-    // Vue.component('video-stage', require('fh-components/video-stage'))
-    // Vue.component('fragment-shader', require('@fuzzco/fragment-shader'))
-    // Vue.component('vue-marquee', require('@fuzzco/vue-marquee'))
-    // Vue.component('scrub-wrap', require('@fuzzco/scrub-wrap'))
-    // Vue.component('super-image', require('@fuzzco/super-image'))
-
-    // Register mixins
-    // Vue.mixin(require('@fuzzco/in-view'))
-    // Vue.mixin(require('~/mixins/head'))
 
     // autoblur
     autoBlur()
