992949bbdf749911852402d565b19841c23b1c59
Sander Moolin <sandermoolin@gmail.com>
2020-01-31 13:59:42 -0500

    Adding query loop in libs/prismic/fetchByType

--- libs/prismic/index.js
+++ libs/prismic/index.js
@@ -1,6 +1,8 @@
 import { sampleSize as _sampleSize } from 'lodash'
 import Prismic from 'prismic-javascript'
 
+const PRISMIC_MAX_PAGES_PER_QUERY = 100
+
 // helper to init API
 const CACHE_TIME = 3 * 60 * 1000
 let api, stamp
@@ -23,7 +25,7 @@ export const fetchByType = async ops => {
             {
                 type: 'page',
                 slug: '',
-                pageSize: 40,
+                pageSize: PRISMIC_MAX_PAGES_PER_QUERY,
                 page: 1,
                 orderings: ''
             },
@@ -40,12 +42,28 @@ export const fetchByType = async ops => {
             return artist
         }
 
-        // run query
-        const { results } = await api.query(predicates, {
-            pageSize: settings.pageSize,
-            page: settings.page,
-            orderings: settings.orderings
-        })
+        // run query (ensures we get all of a desired type - we ran into a bug
+        // where only the first 100 were retrieved, which this loop fixes)
+        const results = []
+        let latestQuery = null
+        let page = 1
+        do {
+            // run query against current page of results
+            latestQuery = await api.query(predicates, {
+                pageSize: settings.pageSize,
+                page: page++,
+                orderings: settings.orderings
+            })
+            // if we get a valid result, add that result to our master output
+            if (latestQuery && Array.isArray(latestQuery.results)) {
+                results.push(...latestQuery.results)
+            }
+        } while (
+            // keep running as long as we have a valid query & maxed-out results lengths
+            latestQuery &&
+            Array.isArray(latestQuery.results) &&
+            latestQuery.results.length === PRISMIC_MAX_PAGES_PER_QUERY
+        )
         return results
     } catch (err) {
         console.log('got an err', err)
