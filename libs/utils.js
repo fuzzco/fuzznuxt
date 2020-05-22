16c8b6c3ebc1f2dd97d83d54b7c1b80474c121dc
Sander Moolin <sandermoolin@gmail.com>
2020-05-22 11:05:46 -0400

    Added waitFor to utils

--- libs/utils.js
+++ libs/utils.js
@@ -7,6 +7,37 @@ export const lerp = (a, b, alpha) => {
 
 export const wait = (time = 1000) => new Promise(res => setTimeout(res, time))
 
+// wait an element to appear in the doc
+export const waitFor = async function(selector, opts = {}) {
+    // set defaults
+    opts = { timeout: 2000, interval: 100, ...opts }
+
+    const lookForEl = () => document.querySelector(selector)
+    let checker
+
+    const checkerPromise = () =>
+        new Promise(res => {
+            checker = setInterval(() => {
+                const el = lookForEl()
+                if (el) {
+                    res(el)
+                }
+            }, opts.interval)
+        })
+
+    const timeout = wait(opts.timeout)
+
+    await Promise.race([checkerPromise(), timeout])
+
+    clearInterval(checker)
+
+    if (el) {
+        return el
+    } else {
+        throw new Error(`couldn't find '${selector}' in ${opts.timeout} ms`)
+    }
+}
+
 export const bezier = (p0, p1, p2, p3) => cubicBezier(p0, p1, p2, p3)
 
 export const scrollUp = (duration = 1000) => {
